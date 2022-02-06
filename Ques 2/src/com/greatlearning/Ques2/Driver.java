e4c9733d2088edd1d066aef4e717c410024b4378
Kapil512 <95181095+Kapil512@users.noreply.github.com>
2022-02-06 22:50:12 +0530

    Add files via upload

---
+++ Ques 2/src/com/greatlearning/Ques2/Driver.java
@@ -0,0 +1,18 @@
+package com.greatlearning.Ques2;
+
+public class Driver {
+	
+		static RightSkewedBinarySearchTree rsbt = new RightSkewedBinarySearchTree();
+		public static void main(String[] args) {
+		RightSkewedBinarySearchTree.Node node = new RightSkewedBinarySearchTree.Node(55);
+        node.left = new RightSkewedBinarySearchTree.Node(30);
+        node.right = new RightSkewedBinarySearchTree.Node(60);
+        node.left.left = new RightSkewedBinarySearchTree.Node(10);
+        node.left.right = new RightSkewedBinarySearchTree.Node(50);
+
+        rsbt.rightSkewedTree(node);
+        rsbt.printTree(rsbt.getRootnode());
+
+	}
+
+}
